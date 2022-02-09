4be8632875bff61340b4fd1d11050c5c10b5a7ab
Aswin3005 <98950106+Aswin3005@users.noreply.github.com>
2022-02-10 01:25:42 +0530

    Add files via upload

---
+++ 3_implementation/code/phone.h
@@ -0,0 +1,14 @@
+#ifndef PHONE_H_INCLUDED
+#define PHONE_H_INCLUDED
+
+void menu();
+void gt();
+void strt();
+void bck();
+void add();
+void list();
+void update();
+void delete();
+void search();
+
+#endif // PHONE_H_INCLUDED
